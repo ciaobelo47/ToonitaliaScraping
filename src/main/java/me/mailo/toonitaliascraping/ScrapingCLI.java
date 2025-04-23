eb3072ec177656daaef821b5866190196a0e1b90
Harly47 <milodipuorto@gmail.com>
2025-04-23 20:04:39 +0200

    Added cookie renew feature, got rid of integrated cookies.txt, updated some paths
    Signed-off-by: Harly47 <milodipuorto@gmail.com>

--- src/main/java/me/mailo/toonitaliascraping/ScrapingCLI.java
+++ src/main/java/me/mailo/toonitaliascraping/ScrapingCLI.java
@@ -15,7 +15,6 @@ import java.awt.*;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.*;
 
@@ -61,7 +60,7 @@ public class ScrapingCLI {
 
             getfromUrlEncr(videoUrl.getDomAttribute("href"));
 
-        } catch (NoSuchElementException | URISyntaxException | IOException | InterruptedException e) {
+        } catch (NoSuchElementException e) {
             throw new RuntimeException(e);
         } finally {
             if (driver != null) {
@@ -71,19 +70,26 @@ public class ScrapingCLI {
         }
     }
 
-    private static void getfromUrlEncr(String url) throws IOException, URISyntaxException, InterruptedException {
-        logger.log(LogLevel.INFO, "Trying to open url encrypter link...", true);
-        driver.get(url);
-        loadCookiesfromFile(driver);
-        logger.log(LogLevel.INFO, "Refreshing page with loaded cookies...", true);
-        driver.navigate().refresh();
+    private static void getfromUrlEncr(String url) {
+        try {
+            logger.log(LogLevel.INFO, "Trying to open url encrypter link...", true);
+            driver.get(url);
+            loadCookiesfromFile(driver);
+            logger.log(LogLevel.INFO, "Refreshing page with loaded cookies...", true);
+            driver.navigate().refresh();
 
-        logger.log(LogLevel.INFO, "Identifying button and click on it...", true);
-        WebElement a = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(By.id("ad_space"), By.tagName("a")));
-        String videoUrl = a.getDomAttribute("href");
-        logger.log(LogLevel.DEBUG, "HREF: " + a.getDomAttribute("href"), true);
+            logger.log(LogLevel.INFO, "Identifying button and click on it...", true);
+            WebElement a = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(By.id("ad_space"), By.tagName("a")));
 
-        getfromUrlVideo(videoUrl);
+            String videoUrl = a.getDomAttribute("href");
+            logger.log(LogLevel.DEBUG, "HREF: " + a.getDomAttribute("href"), true);
+
+            getfromUrlVideo(videoUrl);
+        } catch (TimeoutException | IOException e) {
+            renewCookies(driver.getCurrentUrl());
+        } catch (Exception err) {
+            throw new RuntimeException(err);
+        }
     }
 
     private static void getfromUrlVideo(String url) throws URISyntaxException, IOException {
@@ -102,62 +108,133 @@ public class ScrapingCLI {
         desktop.browse(new URI(url));
     }
 
-    private static void loadCookiesfromFile(WebDriver drv) {
+    private static void loadCookiesfromFile(WebDriver drv) throws IOException {
         logger.log(LogLevel.INFO, "Trying to load cookies...", true);
         int cookiesAdded = 0;
         int linesSkipped = 0;
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Launcher.class.getResourceAsStream("cookies.txt"), StandardCharsets.UTF_8))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.trim().isEmpty() || line.startsWith("#")) {
-                    linesSkipped++;
-                    continue;
+        BufferedReader ps = new BufferedReader(new FileReader("cookies.txt"));
+        String line;
+        while ((line = ps.readLine()) != null) {
+            if (line.trim().isEmpty() || line.startsWith("#")) {
+                linesSkipped++;
+                continue;
+            }
+
+            StringTokenizer tokenizer = new StringTokenizer(line, "\t");
+            if (tokenizer.countTokens() < 7) {
+                linesSkipped++;
+                continue;
+            }
+
+            try {
+                String domain = tokenizer.nextToken();
+                String flag = tokenizer.nextToken();
+                String path = tokenizer.nextToken();
+                boolean isSecure = Boolean.parseBoolean(tokenizer.nextToken());
+                long expiryTimestampSeconds = Long.parseLong(tokenizer.nextToken());
+                String name = tokenizer.nextToken();
+                String value = tokenizer.nextToken();
+
+                boolean isHttpOnly = false;
+                if (domain.startsWith("#HttpOnly_")) {
+                    isHttpOnly = true;
+                    domain = domain.substring("#HttpOnly_".length());
                 }
 
-                StringTokenizer tokenizer = new StringTokenizer(line, "\t");
-                if (tokenizer.countTokens() < 7) {
-                    linesSkipped++;
-                    continue;
+                Date expiry = null;
+                if (expiryTimestampSeconds != 0) {
+                    expiry = new Date(expiryTimestampSeconds * 1000);
                 }
 
+                Cookie.Builder cookieBuilder = new Cookie.Builder(name, value)
+                        .domain(domain)
+                        .path(path)
+                        .expiresOn(expiry)
+                        .isSecure(isSecure)
+                        .isHttpOnly(isHttpOnly);
+
+                Cookie cookie = cookieBuilder.build();
+
+                drv.manage().addCookie(cookie);
+                cookiesAdded++;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        logger.log(LogLevel.DEBUG, "Loading completed! Cookies loaded: " + cookiesAdded + " ; Lines Skipped/Wrong: " + linesSkipped, true);
+    }
+
+    private static void renewCookies(String urltorenew) {
+        logger.log(LogLevel.WARN, "Your cookies have expired. Trying to renew them...", true);
+
+        FirefoxDriver tmpDrv = new FirefoxDriver();
+        WebDriverWait tmpWait = new WebDriverWait(tmpDrv, Duration.ofSeconds(30));
+
+        try {
+            tmpDrv.manage().window().minimize();
+            logger.log(LogLevel.WARN, "A page will open up in 5 seconds, you have to do the verification under 30 seconds", true);
+            Thread.sleep(5000);
+
+            tmpDrv.manage().window().maximize();
+
+            tmpDrv.get(urltorenew);
+            WebElement tmpA = tmpWait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(By.id("ad_space"), By.tagName("a")));
+            if (tmpA.isDisplayed()) {
                 try {
-                    String domain = tokenizer.nextToken();
-                    String flag = tokenizer.nextToken();
-                    String path = tokenizer.nextToken();
-                    boolean isSecure = Boolean.parseBoolean(tokenizer.nextToken());
-                    long expiryTimestampSeconds = Long.parseLong(tokenizer.nextToken());
-                    String name = tokenizer.nextToken();
-                    String value = tokenizer.nextToken();
+                    logger.log(LogLevel.INFO, "Collected new cookies! Formatting and saving them...", true);
 
-                    boolean isHttpOnly = false;
-                    if (domain.startsWith("#HttpOnly_")) {
-                        isHttpOnly = true;
-                        domain = domain.substring("#HttpOnly_".length());
-                    }
+                    formatCookies(tmpDrv.manage().getCookies(), "cookies.txt");
 
-                    Date expiry = null;
-                    if (expiryTimestampSeconds != 0) {
-                        expiry = new Date(expiryTimestampSeconds * 1000);
-                    }
-
-                    Cookie.Builder cookieBuilder = new Cookie.Builder(name, value)
-                            .domain(domain)
-                            .path(path)
-                            .expiresOn(expiry)
-                            .isSecure(isSecure)
-                            .isHttpOnly(isHttpOnly);
-
-                    Cookie cookie = cookieBuilder.build();
-
-                    drv.manage().addCookie(cookie);
-                    cookiesAdded++;
+                    tmpDrv.close();
+                    getfromUrlEncr(urltorenew);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }
-            logger.log(LogLevel.DEBUG, "Loading completed! Cookies loaded: " + cookiesAdded + " ; Lines Skipped/Wrong: " + linesSkipped, true);
-        } catch (IOException e) {
+        } catch (NoSuchElementException e) {
+            logger.log(LogLevel.ERROR, "Failed to renew cookies, the app will close.", true);
+            tmpDrv.close();
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static void formatCookies(Set<Cookie> cookies, String filepath) {
+        try {
+            PrintWriter pw = new PrintWriter(filepath);
+            pw.write("# Netscape HTTP Cookie File\n");
+            pw.write("# This is a generated file! Do not edit.\n");
+            pw.write("\n");
+
+            for (Cookie c : cookies) {
+                String domain = c.getDomain();
+                boolean includeSubdomains = domain.startsWith(".");
+                String path = c.getPath();
+                boolean isSecure = c.isSecure();
+                long expiryTimestampSeconds = (c.getExpiry() != null) ? c.getExpiry().getTime() / 1000 : 0;
+                String name = c.getName();
+                String value = c.getValue();
+                boolean isHttpOnly = c.isHttpOnly();
+
+                // Formato Netscape: domain flag path secure expiry name value
+                pw.write(domain);
+                pw.write("\t");
+                pw.write(includeSubdomains ? "TRUE" : "FALSE");
+                pw.write("\t");
+                pw.write(path);
+                pw.write("\t");
+                pw.write(isSecure ? "TRUE" : "FALSE");
+                pw.write("\t");
+                pw.write(String.valueOf(expiryTimestampSeconds));
+                pw.write("\t");
+                pw.write(name);
+                pw.write("\t");
+                pw.write(value);
+                pw.write("\n");
+            }
+
+            pw.close();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
